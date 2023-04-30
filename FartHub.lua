8382b815e60ff0451b9ffb0060cc778a849e1dee
Ivan Fila <opr103osos@gmail.com>
2023-04-30 18:48:54 +0100

    Update FartHub.lua

--- FartHub.lua
+++ FartHub.lua
@@ -2,5 +2,6 @@ if game.GameId == 9664474819 or 3589987987 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/FartScript.lua"),true))()
 else if game.GameId == 4036302567 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/Alone.lua"),true))()
+else
    game.Players.LocalPlayer:Kick("wrong game babagrill")
 end
