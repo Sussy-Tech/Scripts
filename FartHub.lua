a81db5847797e66895b5332b8ba7ae346e5c180f
NaN <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-05-01 17:10:03 -0300

    Update name of script

--- FartHub.lua
+++ FartHub.lua
@@ -1,5 +1,5 @@
 if game.GameId == 3589987987 then
-   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/FartScript.lua"),true))()
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/BurgerGame.lua"),true))()
 elseif game.GameId == 4036302567 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/Alone.lua"),true))()
 else
