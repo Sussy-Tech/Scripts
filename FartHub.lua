eb00d9da30de3c5d9429639cf92271c42ddfb667
Ivan Fila <opr103osos@gmail.com>
2023-05-02 16:15:16 +0100

    Update FartHub.lua

--- FartHub.lua
+++ FartHub.lua
@@ -4,6 +4,8 @@ elseif game.GameId == 4036302567 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/Alone.lua"),true))()
 elseif game.GameId == 3148127455 then 
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/Get_A_Drink_At_3AM.lua"),true))()
+elseif game.GameId == 992334288 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/GamesUniteTestingPlace.lua"),true))()
 else
    print("wrong game")
 end
