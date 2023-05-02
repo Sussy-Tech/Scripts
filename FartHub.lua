16302f1e522248cf47f700e56cbcbb2b44d845de
NaN <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-05-01 21:28:53 -0300

    Fix game id

--- FartHub.lua
+++ FartHub.lua
@@ -2,7 +2,7 @@ if game.GameId == 3589987987 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/BurgerGame.lua"),true))()
 elseif game.GameId == 4036302567 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/Alone.lua"),true))()
-elseif game.GameId == 8197423034 then 
+elseif game.GameId == 3148127455 then 
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Sussy-Tech/Scripts/main/Get_A_Drink_At_3AM.lua"),true))()
 else
    print("wrong game")
